fdf5c4c47bc043c0a036ee7f038adc8ba5a7e8cf
Leandro Brás <shadowgaming.7@gmail.com>
2023-04-13 20:31:18 +0100

    Wip About me section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,65 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/me_about.png'
+import { FaAward } from 'react-icons/fa'
+import { VscFolderLibrary } from 'react-icons/vsc'
+import { BiAtom } from 'react-icons/bi'
 
 const About = () => {
   return (
-    <section id='about'>About</section>
+    <section id='about'>
+      <h5>Get to know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="About Me" />
+          </div>
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className='about__card'>
+              <FaAward className='about__card-icon' />
+              <h5>Experience</h5>
+              <small>4+ Years</small>
+            </article>
+            <article className='about__card'>
+              <BiAtom className='about__card-icon' />
+              <h5>Tecnologies</h5>
+              <small>C# | C++ | Python | Unity | React</small>
+            </article>
+            <a href="#portfolio">
+              <article className='about__card'>
+                <VscFolderLibrary className='about__card-icon' />
+                <h5>Projects</h5>
+                <small>10+ Completed</small>
+              </article>
+            </a>
+          </div>
+          <p>
+            Hello! I'm Leandro a Programmer/Developer based in Lisbon, Portugal with a big passion for Gameplay Programming.
+            I also have experience in other fields such as AI, AR, VR, and Front/Back-End Development!
+          </p>
+          <p>
+            I enjoy creating things like video games and applications that allow me to think outside the box while using
+            a wide variety of technologies. My goal is to learn as much as possible while providing incredible
+            experiences and keeping the systems behind them as clean and performant as possible.
+          </p>
+          <p>
+            I have a bachelor's degree in VideoGames from <a href="https://www.ulusofona.pt/" target="blank">Lusófona University</a>,
+            in which I developed several applications throughout the last 4 years, ranging from mobile apps, AR apps, VR
+            games, Online Games, and even developing my own mini game engine for the Windows CMD. Having already
+            previously obtained an EQF level 4 in Programming and Management of Computer Systems from
+            <a href="https://ae-ginestalmachado.pt/">AEGM</a>.
+          </p>
+          <p>
+            I'm currently in the process of finishing my Master's degree in Computer Engineering!
+          </p>
+          <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+        </div>
+      </div>
+    </section>
   )
 }
 
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -3,18 +3,25 @@ import './nav.css'
 import {IoHome} from 'react-icons/io5'
 import {FaUser, FaBook, FaGamepad} from 'react-icons/fa'
 import {RiServiceFill, RiMessage2Fill} from 'react-icons/ri'
+import {useState} from 'react'
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#');
   return (
     <nav>
-      <a href="#"><IoHome /></a>
-      <a href="#about"><FaUser /></a>
-      <a href="#experience"><FaBook /></a>
-      <a href="#services"><RiServiceFill /></a>
-      <a href="#portfolio"><FaGamepad /></a>
-      <a href="#contact"><RiMessage2Fill /></a>
+      <a href="#" onClick={() => setActiveNav('#')} className={getActiveState(activeNav, '#')}><IoHome /></a>
+      <a href="#about" onClick={() => setActiveNav('#about')} className={getActiveState(activeNav, '#about')}><FaUser /></a>
+      <a href="#experience" onClick={() => setActiveNav('#experience')} className={getActiveState(activeNav, '#experience')}><FaBook /></a>
+      <a href="#services" onClick={() => setActiveNav('#services')} className={getActiveState(activeNav, '#services')}><RiServiceFill /></a>
+      <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={getActiveState(activeNav, '#portfolio')}><FaGamepad /></a>
+      <a href="#contact" onClick={() => setActiveNav('#contact')} className={getActiveState(activeNav, '#contact')}><RiMessage2Fill /></a>
     </nav>
   )
+}
+
+function getActiveState(activeNav, state)
+{
+  return activeNav === state ? 'active' : '';
 }
 
 export default Nav
